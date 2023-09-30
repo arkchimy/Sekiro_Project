928f612596a4af3164385e0f1dd7e64e355343fc
arkchimy <ajffkeneod12@naver.com>
2023-09-30 23:57:44 +0900

    [Add] Onyscidus Combat

--- Source/Prtfolio_12_24/Strategy_Pattern/Patrol_Strategy.cpp
+++ Source/Prtfolio_12_24/Strategy_Pattern/Patrol_Strategy.cpp
@@ -13,11 +13,27 @@ void UPatrol_Strategy::Action(class UDataTable* Montage_Table, class AGameActor*
 {
 	CLog::Log("Patrol_Action");//operation변경은 Notify로
 	
+	int ran = rand() % 5;
+	switch (ran)
+	{
+	case 0 : 
+		direction = EMouseBut::Right;
+		break;
+	case 1 : 
+		direction = EMouseBut::Left;
+		break;
+	case 2:
+		direction = EMouseBut::Back;
+		break;
+	default:
+		direction = EMouseBut::Front;
+		
+	}
 	TArray<FName> Data_size = Montage_Table->GetRowNames();
 	for (int i = 0; i < Data_size.Num(); i++)
 	{
 		FWeaponData_Enemy temp = *(Montage_Table->FindRow<FWeaponData_Enemy>(Data_size[i], Data_size[i].ToString()));
-		if (temp.Input_But == direction)
+		if (temp.Input_But == direction && temp.Combat == ECombat_Monster::Partrol)
 		{
 			OwnerActor->PlayAnimMontage(temp.Data.Montage, temp.Data.PlayRatio, temp.Data.StartSection);
 			return;
